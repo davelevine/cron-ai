6fadcf110e4969e5fa4e347a31566b3ab484b1b4
davelevine <dave@davelevine.io>
2023-11-14 20:48:48 -0500

    Personalized app; added Dracula theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
+import "dracula-prism/dist/css/dracula-prism.css"; // Import Dracula theme
 import "./globals.css";
 
-import { Inter } from "@next/font/google";
-import { AnalyticsWrapper } from "./components/analytics";
+import { Inter } from "next/font/google";
 import { FaGithub, FaTwitter } from "react-icons/fa";
 import Link from "next/link";
 
@@ -12,18 +12,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const bodyStyles = {
+    backgroundColor: "#282a36",
+  };
+
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      {/* ... (head and other elements) */}
       <body
-        className={`${inter.className} h-screen border-neutral-800 bg-neutral-900`}
+        className={`${inter.className} h-screen border-neutral-800`}
+        style={bodyStyles}
       >
         <div className="flex-1">{children}</div>
-        <AnalyticsWrapper />
         <footer className="flex justify-center gap-6 py-12">
           <Link
             href="https://twitter.com/iamdavelevine"
